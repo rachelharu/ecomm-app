70a7882394ab860418536e4f76e2a4270863e5ff
rachelharu <rachel.haru9@gmail.com>
2022-10-19 11:53:23 -0700

    products from db shown on index.js

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -3,12 +3,16 @@ const { check, validationResult } = require('express-validator');
 const multer = require('multer');
 const Product = require('../../repo/products');
 const productsNewTemplate = require('../../views/admin/products/new');
+const productsIndexTemplate = require('../../views/admin/products/index');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', (req, res) => {});
+router.get('/admin/products', async (req, res) => {
+  const products = await Product.find();
+  res.send(productsIndexTemplate({ products }));
+});
 
 router.get('/admin/products/new', (req, res) => {
   res.send(productsNewTemplate({}));
@@ -36,7 +40,7 @@ router.post('/admin/products/new', upload.single('image'), [requireTitle, requir
         console.log(err);
       } else {
         console.log('success');
-        res.send('submitted');
+        res.redirect('/admin/products');
       }
     });
     
