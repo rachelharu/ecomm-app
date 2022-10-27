d722dd69a15a1b1012f7e9ab8813666561932721
rachelharu <rachel.haru9@gmail.com>
2022-10-26 20:23:14 -0700

    create shopping cart and add items to DB

--- routes/carts.js
+++ routes/carts.js
@@ -7,28 +7,29 @@ const router = express.Router();
 let cart;
 
 router.post('/cart/products', async (req, res) => {
-    if (!req.session.cartId) {
-      cart =  await Carts.create({ items: [] });
-      req.session.cartId = cart.id;
-      } else {
-        cart = await Carts.find({ _id: req.session.cartId });
-        console.log(req.session.cartId);
-      }
-      console.log(cart);
+  if (!req.session.cartId) {
+    cart = await Carts.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    cart = await Carts.find({ _id: req.session.cartId });
+    console.log(req.session.cartId);
+  }
+  console.log({ _id: req.body.productId });
 
-      // const existingItem = cart.items.find(
-      //   (item) => item.id === req.body.productId
-      // );
-      // if (existingItem) {
-      //   existingItem.quantity++;
-      // } else {
-      //   cart.items.push({ _id: req.body.productId, quantity: 1 });
-      // }
-      // await Carts.updateOne(cart.id, {
-      //   items: cart.items,
-      // });
-    
- res.send('product added to cart!!');
+
+
+  await Carts.updateOne(cart, {
+    $push: {
+      items: {
+        _id: req.body.productId,
+        quantity: 1,
+      },
+    },
+  });
+
+  console.log(cart);
+
+  res.send('product added to cart!!');
 });
 
 module.exports = router;
