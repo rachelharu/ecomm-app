5497b8a3e54fc755238cff4e7918edcadec26ae6
rachelharu <rachel.haru9@gmail.com>
2022-10-30 20:50:49 -0700

    refactor code

--- routes/carts.js
+++ routes/carts.js
@@ -7,35 +7,8 @@ const Carts = require('../repo/carts');
 const router = express.Router();
 let cart;
 
-
 router.post('/cart/products', async (req, res) => {
   Carts.findById(req.session.cartId, (err, foundCart) => {
-  if(err) {
-    console.log(err) // This err is for the find by Id, not to the update function
-  }
-  if (foundCart) {
-    console.log(foundCart)
-    console.log(req.body.productId)
-    let check = foundCart.items.map(item => item.id);
-    console.log(check)
-    if( check.includes(req.body.productId)) {
-      console.log('MATCH FOUND')
-    }
-    Carts.updateOne( 
-      { _id:foundCart._id }, {
-      $push: {
-        items: {
-          _id: req.body.productId,
-          quantity: 1,
-        },
-      },
-    },(err, updatedCart) => {
-       if(err){
-         console.log(err)
-       }
-   } 
-  );
-  } else {
     if (!foundCart) {
       const newCart = new Carts({
         _id: req.session.cartId,
@@ -43,7 +16,26 @@ router.post('/cart/products', async (req, res) => {
       });
       newCart.save();
     }
-  } 
+    if (foundCart) {
+      console.log(foundCart);
+      console.log(req.body.productId);
+      let check = foundCart.items.map((item) => item.id);
+      console.log(check);
+      if (check.includes(req.body.productId)) {
+        console.log('MATCH FOUND');
+      }
+      Carts.updateOne(
+        { _id: foundCart._id },
+        {
+          $push: {
+            items: {
+              _id: req.body.productId,
+              quantity: 1,
+            },
+          },
+        }
+      );
+    }
   });
 
   res.send('product added to cart!!');
