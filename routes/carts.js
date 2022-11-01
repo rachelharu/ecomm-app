4c14d2a523899355f0e9197a0eda5a6a8f2accfb
rachelharu <rachel.haru9@gmail.com>
2022-11-01 13:14:43 -0700

    create get request for cart, create post request for deletion, create html for show.js

--- routes/carts.js
+++ routes/carts.js
@@ -1,42 +1,72 @@
 const express = require('express');
 const Carts = require('../repo/carts');
+const Product = require('../repo/products');
+const cartShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
+let cart;
 
 router.post('/cart/products', async (req, res) => {
-try {
-  let cart = await Carts.findById(req.session.cartId);
-  // Check if cart exists
-  if (!cart) {
-    // Create new cart
-    cart = await Carts.create({
-      _id: req.session.cartId,
-      items: [],
-    });
+  try {
+     cart = await Carts.findById(req.session.cartId);
+    // Check if cart exists
+    if (!cart) {
+      // Create new cart
+      cart = await Carts.create({
+        _id: req.session.cartId,
+        items: [],
+      });
+    }
+
+    // Check if product is present
+    const productIndex = cart.items.findIndex(
+      (item) => item._id === req.body.productId
+    );
+    if (productIndex === -1) {
+      // Create new product
+      cart.items.push({
+        _id: req.body.productId,
+        quantity: 1,
+      });
+    } else {
+      // Update existing product
+      cart.items[productIndex].quantity += 1;
+    }
+    console.log(cart);
+    // Save updated cart
+    await cart.save();
+
+    res.redirect('/cart');
+    // res.status(200).send('product added to cart!!');
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('there was an error');
+  } 
+});
+
+router.get('/cart', async (req, res) => {
+  if (!req.session.cartId) {
+    return res.redirect('/');
   }
+  const cart = await Carts.findById(req.session.cartId);
+
+  for (let item of cart.items) {
+    const currentProduct = await Product.findById(item._id);
+
+    item.product = currentProduct;
+  }
+
+  res.send(cartShowTemplate({ items: cart.items }));
+});
+
+router.post('/cart/products/delete', async (req, res) => {
+  const { itemId } = req.body;
+  const cart = await Carts.findById(req.session.cartId);
+
+  const items = cart.items.filter((item) => item._id !== itemId);
+  await Carts.updateOne(cart, { items });
   console.log(cart);
-  // Check if product is present
-  const productIndex = cart.items.findIndex(
-    (item) => item._id === req.body.productId
-  );
-  if (productIndex === -1) {
-    // Create new product
-    cart.items.push({
-      _id: req.body.productId,
-      quantity: 1,
-    });
-  } else {
-    // Update existing product
-    cart.items[productIndex].quantity += 1;
-  }
-
-  // Save updated cart
-  await cart.save();
-
-  res.status(200).send('product added to cart!!');
-} catch (err) {
-  res.status(500).send('there was an error');
-}
+  res.redirect('/cart');
 });
 
 module.exports = router;
